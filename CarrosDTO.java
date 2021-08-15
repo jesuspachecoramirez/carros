fd738d6d2299bcfea2d125f8f4b8cd713ef08faf
jesuspachecoramirez <88979582+jesuspachecoramirez@users.noreply.github.com>
2021-08-15 11:07:36 -0500

    Create CarrosDTO.java

---
+++ CarrosDTO.java
@@ -0,0 +1,129 @@
+package Modelo;
+
+
+
+
+public class CarrosDTO {
+    private String Ferrari;
+    private String Bmw;
+    private String Mclaren;
+    private String Lamborgini;
+    private String Porche;
+    private String Audi;
+    private String Bugatti;
+    private String Subaru;
+
+    /**
+     * @return the Ferrari
+     */
+    public String getFerrari() {
+        return Ferrari;
+    }
+
+    /**
+     * @param Ferrari the Ferrari to set
+     */
+    public void setFerrari(String Ferrari) {
+        this.Ferrari = Ferrari;
+    }
+
+    /**
+     * @return the Bmw
+     */
+    public String getBmw() {
+        return Bmw;
+    }
+
+    /**
+     * @param Bmw the Bmw to set
+     */
+    public void setBmw(String Bmw) {
+        this.Bmw = Bmw;
+    }
+
+    /**
+     * @return the Mclaren
+     */
+    public String getMclaren() {
+        return Mclaren;
+    }
+
+    /**
+     * @param Mclaren the Mclaren to set
+     */
+    public void setMclaren(String Mclaren) {
+        this.Mclaren = Mclaren;
+    }
+
+    /**
+     * @return the Lamborgini
+     */
+    public String getLamborgini() {
+        return Lamborgini;
+    }
+
+    /**
+     * @param Lamborgini the Lamborgini to set
+     */
+    public void setLamborgini(String Lamborgini) {
+        this.Lamborgini = Lamborgini;
+    }
+
+    /**
+     * @return the Porche
+     */
+    public String getPorche() {
+        return Porche;
+    }
+
+    /**
+     * @param Porche the Porche to set
+     */
+    public void setPorche(String Porche) {
+        this.Porche = Porche;
+    }
+
+    /**
+     * @return the Audi
+     */
+    public String getAudi() {
+        return Audi;
+    }
+
+    /**
+     * @param Audi the Audi to set
+     */
+    public void setAudi(String Audi) {
+        this.Audi = Audi;
+    }
+
+    /**
+     * @return the Bugatti
+     */
+    public String getBugatti() {
+        return Bugatti;
+    }
+
+    /**
+     * @param Bugatti the Bugatti to set
+     */
+    public void setBugatti(String Bugatti) {
+        this.Bugatti = Bugatti;
+    }
+
+    /**
+     * @return the Subaru
+     */
+    public String getSubaru() {
+        return Subaru;
+    }
+
+    /**
+     * @param Subaru the Subaru to set
+     */
+    public void setSubaru(String Subaru) {
+        this.Subaru = Subaru;
+    }
+
+   
+}
